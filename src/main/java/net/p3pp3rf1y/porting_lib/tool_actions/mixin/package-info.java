e1639e10767a3015a8361c5298b184a1241d2fab
Salandora <salandora@gmail.com>
2024-03-18 18:46:23 +0100

    [Improvement]: Cherry picked and combined all build and github action changes

---
+++ src/main/java/net/p3pp3rf1y/porting_lib/tool_actions/mixin/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Implementation code for SophisticatedBackpacks.
+ */
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.porting_lib.tool_actions.mixin;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
