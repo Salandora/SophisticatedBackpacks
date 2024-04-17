38db3c0cbaa32ba5c8db5f622ff4667c90024c12
Salandora <salandora@gmail.com>
2024-04-17 02:54:57 +0200

    [Fix]: Potentially fix incompatiblity with some (unidentified) mods

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/mixin/PiglinAiMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/mixin/PiglinAiMixin.java
@@ -3,11 +3,10 @@ package net.p3pp3rf1y.sophisticatedbackpacks.mixin;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
@@ -17,27 +16,28 @@ import net.p3pp3rf1y.sophisticatedbackpacks.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
 import net.p3pp3rf1y.sophisticatedbackpacks.util.PlayerInventoryProvider;
 
-import java.util.Iterator;
 import java.util.List;
 
 @Mixin(PiglinAi.class)
 public class PiglinAiMixin {
-	@Redirect(method = "isWearingGold", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getArmorSlots()Ljava/lang/Iterable;"))
-	private static Iterable<ItemStack> sophisticatedbackpacks$isWearingGold$getArmorSlots(LivingEntity instance) {
-		if (instance instanceof Player player) {
-			List<ItemStack> trinkets = Lists.newArrayList();
-			PlayerInventoryProvider.get().runOnBackpacks(player, CompatModIds.TRINKETS, (backpack, inventoryHandlerName, identifier, slot) -> trinkets.add(backpack));
-			return Iterables.concat(instance.getArmorSlots(), trinkets);
+	@Inject(method = "isWearingGold", at = @At(value = "HEAD"), cancellable = true)
+	private static void sophisticatedBackpacks$isWearingGold(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+		for (ItemStack itemStack : sophisticatedBackpacks$getArmorSlots(entity)) {
+			if (itemStack.getItem() == ModItems.GOLD_BACKPACK) {
+				cir.setReturnValue(true);
+				return;
+			}
 		}
-
-		return instance.getArmorSlots();
 	}
 
-    @Inject(method = "isWearingGold(Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
-    private static void sophisticatedbackpacks$isWearingGold(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir, Iterable<ItemStack> iterable, Iterator<ItemStack> var2, ItemStack itemStack) {
-        if (itemStack.getItem() == ModItems.GOLD_BACKPACK) {
-            cir.setReturnValue(true);
-            cir.cancel();
-        }
-    }
+	@Unique
+	private static Iterable<ItemStack> sophisticatedBackpacks$getArmorSlots(LivingEntity entity) {
+		if (entity instanceof Player player) {
+			List<ItemStack> trinkets = Lists.newArrayList();
+			PlayerInventoryProvider.get().runOnBackpacks(player, CompatModIds.TRINKETS, (backpack, inventoryHandlerName, identifier, slot) -> trinkets.add(backpack));
+			return Iterables.concat(entity.getArmorSlots(), trinkets);
+		}
+
+		return entity.getArmorSlots();
+	}
 }
