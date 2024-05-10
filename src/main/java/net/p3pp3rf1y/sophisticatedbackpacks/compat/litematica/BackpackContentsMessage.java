a6e352c9e9a5dd97b2b0c46e8372ff456c6032ff
Salandora <salandora@gmail.com>
2024-05-11 01:19:32 +0200

    [Fix]: Fix server incompatibility with reworked Litematica compat

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/litematica/BackpackContentsMessage.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/litematica/BackpackContentsMessage.java
@@ -1,46 +1,23 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.compat.litematica;
 
-import com.google.common.collect.Maps;
+import me.pepperbell.simplenetworking.S2CPacket;
+import me.pepperbell.simplenetworking.SimpleChannel;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.world.entity.player.Player;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackStorage;
 import net.p3pp3rf1y.sophisticatedbackpacks.common.BackpackWrapperLookup;
-import net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaCompat;
-import net.p3pp3rf1y.sophisticatedcore.inventory.InventoryHandler;
-import net.p3pp3rf1y.sophisticatedcore.network.SimplePacketBase;
-import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeHandler;
+import net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaHelper;
 
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class BackpackContentsMessage extends SimplePacketBase {
-	public static BackpackContentsMessage create(List<UUID> uuids) {
-		Map<UUID, CompoundTag> contents = Maps.toMap(uuids, BackpackContentsMessage::getBackpackTag);
-		return new BackpackContentsMessage(contents);
-	}
-	private static CompoundTag getBackpackTag(UUID backpackUuid) {
-		CompoundTag backpackContents = BackpackStorage.get().getOrCreateBackpackContents(backpackUuid);
-
-		CompoundTag inventoryContents = new CompoundTag();
-		Tag inventoryNbt = backpackContents.get(InventoryHandler.INVENTORY_TAG);
-		if (inventoryNbt != null) {
-			inventoryContents.put(InventoryHandler.INVENTORY_TAG, inventoryNbt);
-		}
-		Tag upgradeNbt = backpackContents.get(UpgradeHandler.UPGRADE_INVENTORY_TAG);
-		if (upgradeNbt != null) {
-			inventoryContents.put(UpgradeHandler.UPGRADE_INVENTORY_TAG, upgradeNbt);
-		}
-
-		return inventoryContents;
-	}
-
-
+public class BackpackContentsMessage implements S2CPacket {
 	private final Map<UUID, CompoundTag> backpackContents;
 
 	public BackpackContentsMessage(Map<UUID, CompoundTag> backpackContents) {
@@ -51,25 +28,21 @@ public class BackpackContentsMessage extends SimplePacketBase {
 	}
 
 	@Override
-	public void write(FriendlyByteBuf buffer) {
+	public void encode(FriendlyByteBuf buffer) {
 		buffer.writeMap(this.backpackContents, FriendlyByteBuf::writeUUID, FriendlyByteBuf::writeNbt);
 	}
 
-	@Override
 	@Environment(EnvType.CLIENT)
-	public boolean handle(Context context) {
-		context.enqueueWork(() -> {
-			Player player = context.getClientPlayer();
-			if (player == null || this.backpackContents == null) {
+	@Override
+	public void handle(Minecraft client, ClientPacketListener listener, PacketSender responseSender, SimpleChannel channel) {
+		client.execute(() -> {
+			if (client.player == null || this.backpackContents == null) {
 				return;
 			}
 
 			this.backpackContents.forEach(BackpackStorage.get()::setBackpackContents);
 			BackpackWrapperLookup.invalidateCache();
-			if (LitematicaCompat.getTask() != null) {
-				LitematicaCompat.getTask().incrementReceived(this.backpackContents.size());
-			}
+			LitematicaHelper.incrementReceived(this.backpackContents.size());
 		});
-		return true;
 	}
 }
--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/litematica/LitematicaCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/litematica/LitematicaCompat.java
@@ -1,17 +1,50 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.compat.litematica;
 
+import com.google.common.collect.Maps;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackStorage;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
-import net.p3pp3rf1y.sophisticatedcore.network.PacketHandler;
+import net.p3pp3rf1y.sophisticatedcore.compat.litematica.network.LitematicaPacketHandler;
+import net.p3pp3rf1y.sophisticatedcore.inventory.InventoryHandler;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeHandler;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import static net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems.BACKPACKS;
 import static net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaCompat.REQUEST_CONTENTS_CAPABILITY;
 
 public class LitematicaCompat implements ICompat {
+	public static void alwaysInit() {
+		LitematicaPacketHandler.registerS2CMessage(BackpackContentsMessage.class, BackpackContentsMessage::new);
+		REQUEST_CONTENTS_CAPABILITY.registerForItems((stack, context) -> LitematicaCompat::createBackpackContentsMessage, BACKPACKS);
+	}
+
+	public static BackpackContentsMessage createBackpackContentsMessage(List<UUID> uuids) {
+		Map<UUID, CompoundTag> contents = Maps.toMap(uuids, LitematicaCompat::getBackpackTag);
+		return new BackpackContentsMessage(contents);
+	}
+	private static CompoundTag getBackpackTag(UUID backpackUuid) {
+		CompoundTag backpackContents = BackpackStorage.get().getOrCreateBackpackContents(backpackUuid);
+
+		CompoundTag inventoryContents = new CompoundTag();
+		Tag inventoryNbt = backpackContents.get(InventoryHandler.INVENTORY_TAG);
+		if (inventoryNbt != null) {
+			inventoryContents.put(InventoryHandler.INVENTORY_TAG, inventoryNbt);
+		}
+		Tag upgradeNbt = backpackContents.get(UpgradeHandler.UPGRADE_INVENTORY_TAG);
+		if (upgradeNbt != null) {
+			inventoryContents.put(UpgradeHandler.UPGRADE_INVENTORY_TAG, upgradeNbt);
+		}
+
+		return inventoryContents;
+	}
+
+
 	@Override
 	public void setup() {
-		// Register this on the SophisticatedCore channel
-		PacketHandler.registerS2CMessage(BackpackContentsMessage.class, BackpackContentsMessage::new);
-
-		REQUEST_CONTENTS_CAPABILITY.registerForItems((stack, context) -> BackpackContentsMessage::create, BACKPACKS);
 	}
 }
