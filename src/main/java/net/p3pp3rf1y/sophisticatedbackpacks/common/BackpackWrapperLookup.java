52d18a78bc4f284f741496ade1afa1beb54b6f79
Salandora <salandora@gmail.com>
2024-05-13 00:12:19 +0200

    Revert "[Improvement]: Move Storage Wrappers to SophisticatedCore for better compatibility between soph mods"
    This reverts commit f6137f4c

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/BackpackWrapperLookup.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/BackpackWrapperLookup.java
@@ -3,25 +3,26 @@ package net.p3pp3rf1y.sophisticatedbackpacks.common;
 import com.google.common.collect.MapMaker;
 import team.reborn.energy.api.EnergyStorage;
 
+import net.fabricmc.fabric.api.lookup.v1.item.ItemApiLookup;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
 import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.BackpackWrapper;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.IBackpackWrapper;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModBlocks;
-import net.p3pp3rf1y.sophisticatedcore.common.CapabilityWrapper;
 
 import java.util.Map;
 import java.util.Optional;
 
 import static net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems.BACKPACKS;
-import static net.p3pp3rf1y.sophisticatedcore.common.CapabilityWrapper.STORAGE_WRAPPER_CAPABILITY;
 
 public class BackpackWrapperLookup {
+    public static final ItemApiLookup<IBackpackWrapper, Void> ITEM = ItemApiLookup.get(SophisticatedBackpacks.getRL("item_backpack_wrapper"), IBackpackWrapper.class, Void.class);
 	private static final Map<ItemStack, BackpackWrapper> WRAPPERS = new MapMaker().weakKeys().weakValues().makeMap();
 
     public static Optional<IBackpackWrapper> get(ItemStack provider) {
-        return CapabilityWrapper.get(provider, IBackpackWrapper.class);
+        return Optional.ofNullable(ITEM.find(provider, null));
     }
 
 	public static void invalidateCache() {
@@ -29,12 +30,7 @@ public class BackpackWrapperLookup {
 	}
 
     static {
-        STORAGE_WRAPPER_CAPABILITY.registerForItems((itemStack, context) -> {
-			if (itemStack.getCount() == 1) {
-				return WRAPPERS.computeIfAbsent(itemStack, BackpackWrapper::new);
-			}
-			return null;
-		}, BACKPACKS);
+		BackpackWrapperLookup.ITEM.registerForItems((itemStack, context) -> WRAPPERS.computeIfAbsent(itemStack, BackpackWrapper::new), BACKPACKS);
 
         ItemStorage.SIDED.registerForBlockEntity((blockEntity, direction) -> blockEntity.getCapability(ItemStorage.SIDED, direction), ModBlocks.BACKPACK_TILE_TYPE);
         FluidStorage.SIDED.registerForBlockEntity((blockEntity, direction) -> blockEntity.getCapability(FluidStorage.SIDED, direction), ModBlocks.BACKPACK_TILE_TYPE);
