ce582e77278e404607f6ca0484cb711109a1a83a
Salandora <salandora@gmail.com>
2024-05-13 23:56:27 +0200

    [Fix]: Reworked Litematica compat again due to reverting the Storage Wrapper move commits

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/litematica/LitematicaCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/litematica/LitematicaCompat.java
@@ -1,32 +1,29 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.compat.litematica;
 
-import com.google.common.collect.Maps;
-
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackStorage;
+import net.p3pp3rf1y.sophisticatedbackpacks.common.BackpackWrapperLookup;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+import net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaCompat.LitematicaWrapper;
 import net.p3pp3rf1y.sophisticatedcore.compat.litematica.network.LitematicaPacketHandler;
 import net.p3pp3rf1y.sophisticatedcore.inventory.InventoryHandler;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeHandler;
 
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import static net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems.BACKPACKS;
-import static net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaCompat.REQUEST_CONTENTS_CAPABILITY;
+import static net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaCompat.LITEMATICA_CAPABILITY;
 
 public class LitematicaCompat implements ICompat {
 	public static void alwaysInit() {
 		LitematicaPacketHandler.registerS2CMessage(BackpackContentsMessage.class, BackpackContentsMessage::new);
-		REQUEST_CONTENTS_CAPABILITY.registerForItems((stack, context) -> LitematicaCompat::createBackpackContentsMessage, BACKPACKS);
+		LITEMATICA_CAPABILITY.registerForItems((stack, context) ->
+						BackpackWrapperLookup.get(stack).map(wrapper -> new LitematicaWrapper(wrapper, (uuid) -> new BackpackContentsMessage(uuid, getBackpackTag(uuid))))
+								.orElse(null),
+				BACKPACKS);
 	}
 
-	public static BackpackContentsMessage createBackpackContentsMessage(List<UUID> uuids) {
-		Map<UUID, CompoundTag> contents = Maps.toMap(uuids, LitematicaCompat::getBackpackTag);
-		return new BackpackContentsMessage(contents);
-	}
 	private static CompoundTag getBackpackTag(UUID backpackUuid) {
 		CompoundTag backpackContents = BackpackStorage.get().getOrCreateBackpackContents(backpackUuid);
 
