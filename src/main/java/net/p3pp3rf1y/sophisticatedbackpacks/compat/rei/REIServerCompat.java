43d8d4af9d8fa6a80b680cf1b088e265d0d82d3a
Salandora <salandora@gmail.com>
2025-04-16 17:25:09 +0200

    [Fix]: Fix a bug with REI deleting items when using auto fill

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/rei/REIServerCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/rei/REIServerCompat.java
@@ -3,25 +3,18 @@ package net.p3pp3rf1y.sophisticatedbackpacks.compat.rei;
 import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
 import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
-import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
-import me.shedaniel.rei.api.common.transfer.info.simple.SimpleMenuInfoProvider;
-import me.shedaniel.rei.plugin.common.BuiltinPlugin;
 import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.item.ItemStack;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackItem;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.BackpackWrapper;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.IBackpackWrapper;
-import net.p3pp3rf1y.sophisticatedbackpacks.common.gui.BackpackContainer;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
-import net.p3pp3rf1y.sophisticatedcore.compat.rei.ReiGridMenuInfo;
 import net.p3pp3rf1y.sophisticatedcore.init.ModCoreDataComponents;
 
 import java.util.function.Supplier;
 
-import static net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems.*;
-
-public class REICompat implements REIServerPlugin {
+public class REIServerCompat implements REIServerPlugin {
     @Override
     public double getPriority() {
         return 0D;
@@ -38,13 +31,5 @@ public class REICompat implements REIServerPlugin {
         };
 
         registry.register(colorTag, ModItems.BACKPACKS.stream().map(Supplier::get).toArray(BackpackItem[]::new));
-    }
-
-    @Override
-    public void registerMenuInfo(MenuInfoRegistry registry) {
-        registry.register(BuiltinPlugin.CRAFTING, BackpackContainer.class, SimpleMenuInfoProvider.of(ReiGridMenuInfo::crafting));
-        // TODO: Fix this
-        //registry.register(BuiltinPlugin.CRAFTING, BackpackContainer.class, SimpleMenuInfoProvider.of(ReiGridMenuInfo::stonecutting));
-        //registry.register(BuiltinPlugin.CRAFTING, BackpackContainer.class, SimpleMenuInfoProvider.of(ReiGridMenuInfo::smithing));
     }
 }
