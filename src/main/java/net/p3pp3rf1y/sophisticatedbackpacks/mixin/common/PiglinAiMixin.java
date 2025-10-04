708ed54424cb85156df39a22fe9d246a9ca49ebe
Salandora <salandora@gmail.com>
2025-10-04 16:26:13 +0200

    [Chore]: Moved PiglinAi mixin to sophisticatedLibrary and updated the mixin in general

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/mixin/common/PiglinAiMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/mixin/common/PiglinAiMixin.java
@@ -2,6 +2,8 @@ package net.p3pp3rf1y.sophisticatedbackpacks.mixin.common;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
 import net.minecraft.world.entity.player.Player;
@@ -9,33 +11,26 @@ import net.minecraft.world.item.ItemStack;
 import net.p3pp3rf1y.sophisticatedbackpacks.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedbackpacks.util.PlayerInventoryProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
 @Mixin(PiglinAi.class)
 public class PiglinAiMixin {
-	@Inject(method = "isWearingGold", at = @At(value = "HEAD"), cancellable = true)
-	private static void sophisticatedBackpacks$isWearingGold(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
-		for (ItemStack itemStack : sophisticatedBackpacks_getArmorSlots(entity)) {
-			if (itemStack.sophisticatedLibrary_makesPiglinsNeutral(entity)) {
-				cir.setReturnValue(true);
-				return;
-			}
-		}
-	}
-
-	@Unique
-	private static Iterable<ItemStack> sophisticatedBackpacks_getArmorSlots(LivingEntity entity) {
-		if (entity instanceof Player player) {
+	@ModifyExpressionValue(
+			method = "isWearingGold",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/entity/LivingEntity;getArmorAndBodyArmorSlots()Ljava/lang/Iterable;"
+			)
+	)
+	private static Iterable<ItemStack> sophisticatedBackpacks$injectTrinketBackpacks(Iterable<ItemStack> original, @Local(argsOnly = true) LivingEntity livingEntity) {
+		if (livingEntity instanceof Player player) {
 			List<ItemStack> trinkets = Lists.newArrayList();
 			PlayerInventoryProvider.get().runOnBackpacks(player, CompatModIds.TRINKETS, (backpack, inventoryHandlerName, identifier, slot) -> trinkets.add(backpack));
-			return Iterables.concat(entity.getArmorSlots(), trinkets);
+			return Iterables.concat(original, trinkets);
 		}
 
-		return entity.getArmorSlots();
+		return original;
 	}
 }
