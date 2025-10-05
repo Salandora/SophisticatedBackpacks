2cb8707835beaebc6111cf82a64785dc7fa8f3df
Salandora <salandora@gmail.com>
2025-10-05 02:58:38 +0200

    [Chore]: Moved SophisticatedShearable to SophisticatedLibrary

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/mixin/common/ShearableBlocksMixin.java
+++
@@ -1,9 +0,0 @@
-package net.p3pp3rf1y.sophisticatedbackpacks.mixin.common;
-
-import net.minecraft.world.level.block.*;
-import net.p3pp3rf1y.sophisticatedbackpacks.api.SophisticatedShearable;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin({ DeadBushBlock.class, LeavesBlock.class, SeagrassBlock.class, TallGrassBlock.class, VineBlock.class, WebBlock.class })
-public class ShearableBlocksMixin implements SophisticatedShearable {
-}
--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/mixin/common/ShearableMixin.java
+++
@@ -1,9 +0,0 @@
-package net.p3pp3rf1y.sophisticatedbackpacks.mixin.common;
-
-import net.minecraft.world.entity.Shearable;
-import net.p3pp3rf1y.sophisticatedbackpacks.api.SophisticatedShearable;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Shearable.class)
-public interface ShearableMixin extends SophisticatedShearable {
-}
